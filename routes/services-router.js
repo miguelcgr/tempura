051ab5a73ca43904bdf2e49e684a837d5a4735d5
Javi Sastre <javisastrecal@gmail.com>
2021-02-16 14:08:23 +0100

    service edition implemented

--- routes/services-router.js
+++ routes/services-router.js
@@ -37,18 +37,42 @@ servicesRouter.get("/", (req, res, next) => {
     .catch((err) => console.log(err));
 });
 
-servicesRouter.get("/profile/:id", isLoggedIn, (req, res, next) => {
+servicesRouter.get("/profile/:id", (req, res, next) => {
   const serviceId = req.params.id;
-  Service.findById(serviceId)
-    .populate("giverUser")
-    .then((foundService) => {
-      const data = {
-        isLogNav: isLogNavFn(req),
-        service: foundService,
-      };
-      res.render("service-profile", data);
-    })
-    .catch((err) => console.log(err));
+
+  if (req.session.currentUser.services.includes(serviceId)) {
+    Service.findById(serviceId)
+      .then((foundService) => {
+        const data = {
+          isLogNav: isLogNavFn(req),
+          service: foundService,
+        };
+        res.render("service-profile-own", data);
+      })
+      .catch((err) => console.log(err));
+  } else if (req.session.currentUser) {
+    Service.findById(serviceId)
+      .populate("giverUser")
+      .then((foundService) => {
+        const data = {
+          isLogNav: isLogNavFn(req),
+          service: foundService,
+        };
+        res.render("service-profile-private", data);
+      })
+      .catch((err) => console.log(err));
+  } else {
+    Service.findById(serviceId)
+      .populate("giverUser")
+      .then((foundService) => {
+        const data = {
+          isLogNav: isLogNavFn(req),
+          service: foundService,
+        };
+        res.render("service-profile-public", data);
+      })
+      .catch((err) => console.log(err));
+  }
 });
 
 servicesRouter.get("/create", isLoggedIn, (req, res, next) => {
@@ -71,6 +95,30 @@ servicesRouter.post("/create", (req, res, next) => {
     .then((data) => {
       console.log(data);
       res.render("/users/my-profile");
+    })
+    .catch((err) => console.log(err));
+});
+
+servicesRouter.get("/profile/:id/edit", isLoggedIn, (req, res, next) => {
+  const serviceId = req.params.id;
+  Service.findById(serviceId)
+    .then((foundService) => {
+      const data = {
+        isLogNav: isLogNavFn(req),
+        service: foundService,
+      };
+      res.render("service-profile-edit", data);
+    })
+    .catch((err) => console.log(err));
+});
+
+servicesRouter.post("/edit/:id", isLoggedIn, (req, res, next) => {
+  const serviceId = req.params.id;
+  const { name, description, location, duration, category } = req.body;
+  const incomingData = { name, description, location, duration, category };
+  Service.findByIdAndUpdate(serviceId, incomingData, { new: true })
+    .then(() => {
+      res.redirect(`/services/profile/${serviceId}`);
     })
     .catch((err) => console.log(err));
 });
