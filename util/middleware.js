085b3ca703e6aeefc7396dec0175b3c764b0097b
Javi Sastre <javisastrecal@gmail.com>
2021-02-18 11:57:50 +0100

    navBar middlewares v1

--- util/middleware.js
+++ util/middleware.js
@@ -1,14 +1,23 @@
-function isLoggedIn (req, res, next) {
-    if (req.session.currentUser) {
-        next();
-    }
-    else {
-        res.redirect('/login')
-    }
+function isLoggedIn(req, res, next) {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
 }
 
-
+function setLocals(req, res, next) {
+  if (req.session.currentUser) {
+    User.findById(req.session.currentUser._id)
+      .then((myUser) => {
+        res.locals.userData = myUser;
+      })
+      .catch((err) => console.log(err));
+  }
+  next();
+}
 
 module.exports = {
-    isLoggedIn
-}
+  isLoggedIn,
+  setLocals,
+};
